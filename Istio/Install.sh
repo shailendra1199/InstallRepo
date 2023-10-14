6c12dc29775f3ffb17f5dfc86d889a975b4a9fb5
Shailendra Singh Gaur <83405207+shailendra1199@users.noreply.github.com>
2023-10-15 01:18:00 +0530

    Update Install.sh

--- Istio/Install.sh
+++ Istio/Install.sh
@@ -1,3 +1,5 @@
+#URL 
+#https://istio.io/latest/docs/setup/getting-started/#download -->check out for download istio
 curl -L https://istio.io/downloadIstio | sh -
 cd istio-<version-number>
 export PATH=$PWD/bin:$PATH
